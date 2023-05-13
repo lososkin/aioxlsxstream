1c794c0f82235e568784b5fd18efbbdb66759851
Павел Золотов <zolotovpa@sberbank.ru>
2023-05-14 00:57:08 +0300

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='aioxlsxstream',
-    version='1.0.0',
+    version='1.0.1',
     description='Asynchronous xlsx files generator',
     long_description=long_description,
     long_description_content_type="text/markdown",
